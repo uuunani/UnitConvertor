3cac9b4f2d3fc23927535c4d3e31e8acc95a4dff
unani <coder.unani@gmail.com>
2022-07-05 22:19:46 +0900

    style: CSS Reset 추가

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,20 @@ import Area from './pages/Area';
 import DataSize from './pages/DataSize';
 import Time from './pages/Time';
 
+/*
+  TODO:
+  1. 퍼센트 계산 오류 수정
+  2. 데이터사이즈 비트계산 오류 수정
+  3. 아이콘 다시 셋팅
+  4. 모바일 네비 보기좋게 정렬
+  5. 구글애드센스 거부사항 수정
+  6. 사이트 레이아웃? 유지? 수정?
+  7. 반응형에 대한 고민
+  FIXME:
+  1. CSS 초기화
+  2. Header Footer fixed 써야하는 이유? Fixed는 Header에 컨텐츠가 많아지면 맞추기 어려움
+*/
+
 function App() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
